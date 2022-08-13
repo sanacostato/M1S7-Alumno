21e2b1177cd032ebf99d15d67468b51dfe5e98d2
sanacostato <saacosta.dwfsc9@ucamp.utel.edu.mx>
2022-08-13 10:47:52 -0500

    uso let

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
 console.log("Entro al main.js");
+
+let nombre = "Juan";
+console.log (nombre);
+let nombre = "manuel";
+console.log (nombre);
+// con var puedo tener dos variables con el mismo nombre, eso esta mal. con let no pasa, sale error
